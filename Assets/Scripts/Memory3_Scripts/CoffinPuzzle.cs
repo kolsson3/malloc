bde709193366d7083eb3b27892ea7cfadfee566b
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2021-04-17 16:19:03 -0700

    Memory3 done

---
+++ Assets/Scripts/Memory3_Scripts/CoffinPuzzle.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class CoffinPuzzle : MonoBehaviour
+{
+    public NPCConversation commentDone;
+    public Pickup pickup;
+    public GameObject placedFlowers;
+    public GameObject placedPhoto;
+    public GameObject placedCandle;
+    public GameObject Flowers;
+    public GameObject Photo;
+    public GameObject Candle;
+    public GameObject Dad;
+    public GameObject Ash;
+    public GameObject Portal;
+
+    public bool solved = false;
+
+    public bool checkedCoffin = false;
+
+    public bool doneCandle = false;
+    public bool donePhoto = false;
+    public bool doneFlowers = false;
+
+    private void OnMouseDown()
+    {
+        if (pickup.heldItem == Flowers)
+        {
+            placedFlowers.SetActive(true);
+            Flowers.SetActive(false);
+            doneFlowers = true;
+            pickup.heldItem = null;
+            checkSolved();
+        }
+        else if (pickup.heldItem == Candle)
+        {
+            placedCandle.SetActive(true);
+            Candle.SetActive(false);
+            doneCandle = true;
+            pickup.heldItem = null;
+            checkSolved();
+        }
+        else if (pickup.heldItem == Photo)
+        {
+            placedPhoto.SetActive(true);
+            Photo.SetActive(false);
+            donePhoto = true;
+            pickup.heldItem = null;
+            checkSolved();
+        }
+
+    }
+
+    void checkSolved()
+    {
+        if (doneCandle == true && donePhoto == true && doneFlowers == true)
+        {
+            solved = true;
+            ConversationManager.Instance.StartConversation(commentDone);
+            Dad.SetActive(false);
+            Ash.SetActive(false);
+            Portal.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Memory3_Scripts/Dad_Funeral.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class Dad_Funeral: MonoBehaviour
+{
+    public NPCConversation initConvo;
+    public NPCConversation loopConvo;
+
+    public GameObject coffinPuzzle;
+
+    public bool checkedCoffin = false;
+    public bool doneCoffin = false;
+
+    private void OnMouseDown()
+    {
+        ConversationManager.Instance.StartConversation(initConvo);
+
+        /*if (ConversationManager.Instance == null || !ConversationManager.Instance.IsConversationActive)
+        {
+            if (!init)
+            {
+                init = true;
+                
+                
+                coffeePuzzle.transform.GetChild(0).gameObject.SetActive(true);
+                coffeePuzzle.transform.GetChild(1).gameObject.SetActive(true);
+                coffeePuzzle.transform.GetChild(2).gameObject.SetActive(true);
+
+            }
+            else
+            {
+                ConversationManager.Instance.StartConversation(loopConvo);
+                ConversationManager.Instance.SetBool("needCoffee", needsCoffee);
+                ConversationManager.Instance.SetBool("visitedPantry", hasOpenedPantry);
+                ConversationManager.Instance.SetBool("notVisitedHub", hasSeenHub);
+
+                if (needsCoffee) GameObject.Find("DoorToGarage").GetComponent<Door>().locked = false;
+            }
+        }
+        else if (ConversationManager.Instance != null && ConversationManager.Instance.IsConversationActive)
+        {
+
+        }*/
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coffinPuzzle = GameObject.Find("CoffinPuzzle");
+    }
+
+}
