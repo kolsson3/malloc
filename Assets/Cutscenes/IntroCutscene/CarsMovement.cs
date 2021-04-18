49c6ab66b2989a12950d5ba424f464d046660d7c
Nam Tran <trnam2811@gmail.com>
2021-04-18 14:50:10 -0800

    Finish intro

---
+++ Assets/Cutscenes/IntroCutscene/CarsMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarsMovement : MonoBehaviour
+{
+    public Transform target;
+    public bool isPOVCar;
+    public bool activated = false;
+    public float speed;
+
+    void Update()
+    {
+        if (activated)
+        {
+            // The step size is equal to speed times frame time.
+            float step = speed * Time.deltaTime;
+
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    public void activateTheCar()
+    {
+        activated = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "GoingNorthTrigger" || other.gameObject.name == "GoingNorthTrigger")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Cutscenes/IntroCutscene/CarsTurn.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarsTurn : MonoBehaviour
+{
+    public Transform target;
+    public Transform target2;
+    public bool activated = false;
+    public float speed;
+    public bool turning = false;
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (activated)
+        {
+            if (turning)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -90, 0), Time.deltaTime * 3);
+                transform.position = Vector3.MoveTowards(transform.position, target2.position, 3 * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            }
+        }
+    }
+
+    public void activateTheCar()
+    {
+        activated = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        turning = true;
+    }
+
+}
