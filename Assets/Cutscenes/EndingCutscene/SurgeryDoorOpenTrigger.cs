c8d49274e040a23a0f61b7e511753f3af69bef43
Nam Tran <trnam2811@gmail.com>
2021-04-18 17:40:12 -0800

    Adding outro

---
+++ Assets/Cutscenes/EndingCutscene/SurgeryDoorOpenTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurgeryDoorOpenTrigger : MonoBehaviour
+{
+    public GameObject door;
+    public bool doorOpened = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        doorOpened = true;
+    }
+
+    private void Update()
+    {
+        door.gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * 3);
+    }
+}
