4cbc9bb0286b07d487d927c8b392780b268f7ddd
Nam Tran <trnam2811@gmail.com>
2021-04-18 20:59:36 -0800

    Finished memory2

---
+++ Assets/Scripts/Memory2/BirthdayAnnaDialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class BirthdayAnnaDialogue : MonoBehaviour
+{
+    public NPCConversation initConvo;
+    public NPCConversation loopConvo;
+    bool init = false;
+
+    private void OnMouseDown()
+    {
+        if (!GameObject.Find("GameManager").GetComponent<GameManager>().firstSpeech && (ConversationManager.Instance == null || !ConversationManager.Instance.IsConversationActive))
+        {
+            if (!init)
+            {
+                init = true;
+                ConversationManager.Instance.StartConversation(initConvo);
+
+            }
+            else
+            {
+                ConversationManager.Instance.StartConversation(loopConvo);
+            }
+        }
+    }
+}
