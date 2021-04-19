f541897fd8eb9b14c47ae884b855a400970beff1
kolsson3 <55680535+kolsson3@users.noreply.github.com>
2021-04-18 19:09:56 -0700

    finishing end cutscene

---
+++ Assets/LoadMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadMenu : MonoBehaviour
+{
+    public void Load()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
